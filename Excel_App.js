416e04dbd32ec9a0ed8c4646b21319e89211adfd
Bijay K <bijay.kapali@ey.com>
2021-04-07 12:26:04 -0400

    updated js

--- Excel_App.js
+++ Excel_App.js
@@ -1,11 +1,12 @@
 // The initialize function is required for all add-ins.
+let excel = undefined;
 Office.initialize = function (reason) {
-    
+    excel = Office.context.Excel;
 }
 
 // Create a function for writing to the status div.
 function addHederFooter() {
-    return Excel.run(function (context) {
+    excel?.run(function (context) {
         var range = context.workbook.getSelectedRange();
         range.format.fill.color = "yellow";
         range.load("address");
